b4f78e5d922a1c0264e2d12af85e963636716cc7
DevendranRupesh <rupeshdevendran88@gmail.com>
2023-05-13 23:48:51 +0530

    Conflicts changes

--- src/demo/javatest.java
+++ src/demo/javatest.java
@@ -4,11 +4,15 @@ public class javatest {
 
 public static void main(String[]args) {
 	
-	System.out.println("Test1");
+	System.out.println("Demo Test1");
 	
+<<<<<<< HEAD
 	System.out.println("Test2");
 	
 	System.out.println("Test3");
+=======
+	System.out.println("Demo Test2");
+>>>>>>> refs/remotes/origin/master
 }
 
 
